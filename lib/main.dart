7046d75e0daa5b0419c99a2d7b7213c8c32b2ac8
sreejithcs007 <sreejithcs365@gmail.com>
2024-05-09 12:39:37 +0530

    machine test

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:dio/dio.dart';
-import 'package:go_router/go_router.dart';
+
 
 void main() {
   runApp(MyApp());
